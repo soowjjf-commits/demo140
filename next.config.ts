f1a8c36530b1dd6b56760385b2b7629ab1bd70f1
soowjjf-commits <soowjjf@gmail.com>
2026-01-31 01:01:23 +0530

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  trailingSlash: true,
-  basePath: '/demo140'  // 👈 CHANGE TO YOUR REPO NAME
-}
-
-module.exports = nextConfig
